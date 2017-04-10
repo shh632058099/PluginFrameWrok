05c1a21d02946802bad7dc55d1b0d046cba67765
sunbiao <632058099@qq.com>
2017-04-10 11:15:28 +0800

    add first version of add-in

---
+++ PluginCore/common.mk
@@ -0,0 +1,34 @@
+# This is an automatically generated record.
+# The area between QNX Internal Start and QNX Internal End is controlled by
+# the QNX IDE properties.
+
+ifndef QCONFIG
+QCONFIG=qconfig.mk
+endif
+include $(QCONFIG)
+
+#===== USEFILE - the file containing the usage message for the application. 
+USEFILE=
+
+# Next lines are for C++ projects only
+
+EXTRA_SUFFIXES+=cxx cpp
+
+#===== LDFLAGS - add the flags to the linker command line.
+LDFLAGS+= -fPic -lang-c++
+
+#===== EXTRA_INCVPATH - a space-separated list of directories to search for include files.
+EXTRA_INCVPATH+= \
+	D:\KanziWorkspace_3_2_4_474_92176\Engine\libraries\common\boost\include
+
+include $(MKFILES_ROOT)/qmacros.mk
+ifndef QNX_INTERNAL
+QNX_INTERNAL=$(PROJECT_ROOT)/.qnx_internal.mk
+endif
+include $(QNX_INTERNAL)
+
+include $(MKFILES_ROOT)/qtargets.mk
+
+OPTIMIZE_TYPE_g=none
+OPTIMIZE_TYPE=$(OPTIMIZE_TYPE_$(filter g, $(VARIANTS)))
+
